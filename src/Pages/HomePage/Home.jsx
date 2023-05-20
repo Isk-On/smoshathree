f96944352224caf6f2dde7c99cb87442f35a0ac2
Akmal Lox <akmalbspro@gmail.com>
2023-05-20 13:28:55 +0500

    commit

--- src/Pages/HomePage/Home.jsx
+++ src/Pages/HomePage/Home.jsx
@@ -1,14 +1,44 @@
 import React from "react";
 import Header from "../../components/header/Header";
-import video from "../../assets/gifs/dota.gif"
-import s from "./Home.module.scss"
+import s from "./Home.module.scss";
+import video from "../../assets/gifs/dota.gif";
+import Location from "../../assets/Images/Location.jpg";
+import Cards from "../../components/Cards/Cards";
+import pic from "../../assets/Images/YourMother.jpg"
 
 const Home = () => {
   return (
-  <div className={s.root}>
-    <Header/>
-    <img src={video} alt="" />
-  </div>
+    <div className={s.root}>
+      <Header />
+      <div className={s.Container}>
+        <p>«СОВРЕМЕННЫЙ МНОГОПОЛЬЗОВАТЕЛЬСКИЙ ШЕДЕВР»</p>
+        <p className={s.title}>DESTRUCTOID</p>
+      </div>
+
+      <img className={s.video} src={video} alt="" />
+      <div className={s.Black}>
+        <Cards />
+      </div>
+      <div className={s.contain}>
+        <img className={s.Location} src={Location} alt="" />
+        <p className={s.g}>ПРИСОЕДИНИТЕСЬ К</p>
+        <p className={s.k}>БИТВЕ ДРЕВНИХ</p>
+      </div>
+      <div className={s.black_square}>
+        <p className={s.text}>
+          Каждый день миллионы игроков по всему миру вступают в командную битву
+          5 на 5 в роли одного из более чем сотни героев. Dota — глубочайшая
+          многопользовательская стратегия всех времён, в которой всегда найдётся
+          место новой стратегии или тактике. Она совершенно бесплатна, и это не
+          поменяется — начинайте защищать своего Древнего уже сейчас.
+        </p>
+        <button>Свежие Новости</button>
+
+      </div>
+      <div className={s.content}>
+        <img src={pic} alt="" />
+      </div>
+    </div>
   );
 };
 
---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import s from "./Cards.module.scss"
+import image from "../../assets/Images/scene.jpg"
+
+
+const Cards = () => {
+  return (
+    <div className={s.root}>
+    <div className={s.Card1}>
+        <img className={s.image1} src={image} alt="" />
+    </div>
+    <div className={s.Card2}>
+        <img className={s.image2} src={image} alt="" />
+    </div>
+    <div className={s.Card3}>
+        <img className={s.image3} src={image} alt="" />
+    </div>
+    </div>
+  )
+}
+
+export default Cards
